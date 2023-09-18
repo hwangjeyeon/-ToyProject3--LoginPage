27c3db04493368c64dc9897065b6047f3a380ec6
황제연 <kingpele9018@gmail.com>
2023-09-18 23:23:19 +0900

    인터셉터 등록 파일 추가

---
+++ LoginPage/src/main/java/ToyProject3/LoginPage/WebConfig.java
@@ -0,0 +1,19 @@
+package ToyProject3.LoginPage;
+
+import ToyProject3.LoginPage.Login.LoginInterceptor;
+import ToyProject3.LoginPage.log.LogInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new LoginInterceptor())
+                .order(1)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/", "/login", "/css/**", "/*.ico", "/error");
+    }
+}
